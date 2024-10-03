25275b822c48bba6617fc74da830b1e38fba33b0
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-10-03 15:04:02 -0400

    map functions to arrays

--- src/basics/func.scala
+++ src/basics/func.scala
@@ -4,4 +4,11 @@ object  fn {
     sqrt(2) // a function
     BigInt.probablePrime(100, scala.util.Random) // method
     "Hello".distinct // doesn't use ()
+    "Hello".length // not mutating no ()
+    // we can use (arg) for methods similar to fn calls
+    "Hello"(4) // same as "Hello".apply(4)
+    1.to(10).map(sqrt(_)) // mapping a fun to a range
+    "Rhine".permutations.toArray // all the possible perm
+    "ABC".sum // returns char
+    "A" + "B" + "C" // returns int
 }
